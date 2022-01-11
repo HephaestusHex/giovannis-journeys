f3ae8efe7e7d8b3cfd64038d8a50a9f33baf14db
Le Duck <95136800+duck123acb@users.noreply.github.com>
2022-01-11 14:13:09 -0700

    awsdf

--- artifactStyles.js
+++ artifactStyles.js
@@ -1,7 +1,9 @@
 const tweetsDisplay = document.getElementById("tweets");
-const tweets = ["tweet_1.png", "tweet_2.png", "tweet_3.png", "tweet_4.png", "tweet_5.png", "tweet_6.png", "tweet_7.png", "tweet_8.png"];
+var tweets = ["tweet_1.png", "tweet_2.png", "tweet_3.png", "tweet_4.png", "tweet_5.png", "tweet_6.png", "tweet_7.png", "tweet_8.png"];
+var num = 0
 
 tweetsDisplay.addEventListener("click", function() {
-    console.log("hello")
-    tweetsDisplay.src = `${tweets[1]}`;
+    num++;
+    console.log(num)
+    tweetsDisplay.src = `${tweets[num]}`;
 });
