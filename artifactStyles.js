a1894b2a37b714601a82608f214a0f027ad688e6
Le Duck <95136800+duck123acb@users.noreply.github.com>
2022-01-11 13:59:49 -0700

    sdzxfg

--- artifactStyles.js
+++ artifactStyles.js
@@ -1,7 +1,7 @@
 const tweetsDisplay = document.getElementById("tweets");
 const tweets = ["tweet_1", "tweet_2", "tweet_3", "tweet_4", "tweet_5", "tweet_6", "tweet_7", "tweet_8"];
 
-tweetsDisplay.addEventListener("click", function() {
-    console.log("hello")
-    tweetsDisplay.src = "tweet_2.png";
-});
+// tweetsDisplay.addEventListener("click", function() {
+//     console.log("hello")
+//     tweetsDisplay.src = "tweet_2.png";
+// });
