b58863d682ef2b4c7289f1731c187b3955575fee
Le Duck <95136800+duck123acb@users.noreply.github.com>
2022-01-11 13:53:06 -0700

    ee

---
+++ artifactStyles.js
@@ -0,0 +1,6 @@
+const tweetsDisplay = document.getElementById("tweets");
+var tweets = ["tweet_1", "tweet_2", "tweet_3", "tweet_4", "tweet_5", "tweet_6", "tweet_7", "tweet_8"];
+
+tweetsDisplay.addEventListener("click", function() {
+    tweetsDisplay.src = tweets[1];
+});
