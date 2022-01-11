4f19e233eab13b42f19cc7ab75a78baad0dacc59
Le Duck <95136800+duck123acb@users.noreply.github.com>
2022-01-11 13:55:58 -0700

    aesfd

--- artifactStyles.js
+++ artifactStyles.js
@@ -1,5 +1,5 @@
 const tweetsDisplay = document.getElementById("tweets");
-var tweets = ["tweet_1", "tweet_2", "tweet_3", "tweet_4", "tweet_5", "tweet_6", "tweet_7", "tweet_8"];
+const tweets = ["tweet_1", "tweet_2", "tweet_3", "tweet_4", "tweet_5", "tweet_6", "tweet_7", "tweet_8"];
 
 tweetsDisplay.addEventListener("click", function() {
     console.log("hello")
