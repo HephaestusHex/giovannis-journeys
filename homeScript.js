98b7126de07b7421eac9084d35f7a6cba1415a0e
Le Duck <95136800+duck123acb@users.noreply.github.com>
2022-01-12 14:46:48 -0700

    adge

--- homeScript.js
+++
@@ -1,6 +0,0 @@
-const btn = document.getElementById("no-click");
-const img = document.getElementById("bobing-title");
-
-btn.addEventListener("click", function() {
-    img.style.backgroundImage = "url('https://www.google.com/imgres?imgurl=https%3A%2F%2Foutrightgames.com%2Fwp-content%2Fuploads%2F2021%2F05%2FKEY-ART-PEPPA-PIG-1.png&imgrefurl=https%3A%2F%2Foutrightgames.com%2Fen-ca%2Fgames%2Fmy-friend-peppa-pig%2F&tbnid=zPEMmeqGm2awiM&vet=12ahUKEwiW_rXil631AhX4ADQIHTOtA18QMygFegUIARDfAQ..i&docid=tmyrl4IQBVibsM&w=2000&h=1000&itg=1&q=peppa%20pig&ved=2ahUKEwiW_rXil631AhX4ADQIHTOtA18QMygFegUIARDfAQ')";
-});
