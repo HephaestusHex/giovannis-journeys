8f31822891fd946e14f17b88ea9656f85e10fb20
Le Duck <95136800+duck123acb@users.noreply.github.com>
2022-01-12 07:49:25 -0700

    oops

--- artifactScripts.js
+++ artifactScripts.js
@@ -1,1 +1,12 @@
-var imgs = ["tweet_1.png", "tweet_2", "tweet_3.png", "tweet_4", "tweed_5", "tweet_6", "tweet_7", "tweet_8"];
+const tweetsDisplay = document.getElementById("tweets");
+var tweets = ["tweet_1.png", "tweet_2.png", "tweet_3.png", "tweet_4.png", "tweet_5.png", "tweet_6.png", "tweet_7.png", "tweet_8.png"];
+var num = 0
+
+tweetsDisplay.addEventListener("click", function() {
+    if (num === 7) {
+        num = 0;
+    } else {
+        num++;
+    }
+    tweetsDisplay.src = `${tweets[num]}`;
+});
